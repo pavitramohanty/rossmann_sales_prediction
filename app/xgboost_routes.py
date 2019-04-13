987516f8778ea251c76fd0c50534a2dfd1562b51
pavitra mohanty <pavitramohanty111@gmail.com>
2019-04-13 23:41:03 +0530

    Update xgboost_routes.py

--- app/xgboost_routes.py
+++ app/xgboost_routes.py
@@ -46,7 +46,7 @@ def home():
     return flask.render_template('index.html')
 global day
 global month
-global store1
+global store
 global predictedvalue
 @app.route('/predict', methods=['POST','GET'])
 def make_prediction():
@@ -64,8 +64,8 @@ def make_prediction():
         assortment = int(request.form['Assortment'])
         storeType = int(request.form['StoreType'])
         store = int(request.form['store'])
-        store1=store
         
+       
         # one-hot encode categorical variables
         stateH_encode = onehotCategorical(stateH, 4)
         assortment_encode = onehotCategorical(assortment, 3)
@@ -109,7 +109,7 @@ def make_prediction():
 
 @app.route('/plot',methods=['POST'])
 def build_plot():
-    meansales,meansales16=meansales_of_past_years(store1,month)
+    meansales,meansales16=meansales_of_past_years(store,month)
     img = io.BytesIO()
     data=pd.DataFrame({
             'y':[int(meansales16),int(meansales),float(predictedvalue)],
